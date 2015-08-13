fd9e0f805a6c779424ae29b63c3c8f9a75a6ff22
Valentina <valen92_@hotmail.com>
2015-08-12 20:04:48 -0500

    Antonio organiza consulta herramientas y repuestos - usuario administrador - segunda subida

--- src/java/controller/RepuestosxempresasFacade.java
+++ src/java/controller/RepuestosxempresasFacade.java
@@ -7,9 +7,13 @@
 package controller;
 
 import entities.Repuestosxempresas;
+import entities.Repuestos;
+import entities.Linearepuestos;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -27,6 +31,33 @@ public class RepuestosxempresasFacade extends AbstractFacade<Repuestosxempresas>
 
     public RepuestosxempresasFacade() {
         super(Repuestosxempresas.class);
+    }
+    
+    public List<Repuestosxempresas> findporTipoRepuestoM(int[] range, int idTR) {
+        String consulta = "SELECT n FROM Repuestosxempresas n LEFT JOIN Repuestos h where n.fkidRepuestos.idRepuestos = h.idRepuestos and h.fkidLineaRepuestos.idLineaRepuestos = :idLinea";
+        Query q = getEntityManager().createQuery(consulta);
+        q.setParameter("idLinea", idTR); //Variable a pasar de la sesión
+        q.setMaxResults(range[1] - range[0] + 1);
+        q.setFirstResult(range[0]);
+        return q.getResultList();
+    }
+    
+    public List<Repuestosxempresas> findporTipoRepuestoE(int[] range, int idTR) {
+        String consulta = "SELECT n FROM Repuestosxempresas n LEFT JOIN Repuestos h where n.fkidRepuestos.idRepuestos = h.idRepuestos and h.fkidLineaRepuestos.idLineaRepuestos = :idLinea";
+        Query q = getEntityManager().createQuery(consulta);
+        q.setParameter("idLinea", idTR); //Variable a pasar de la sesión
+        q.setMaxResults(range[1] - range[0] + 1);
+        q.setFirstResult(range[0]);
+        return q.getResultList();
+    }
+    
+    public List<Repuestosxempresas> findporTipoRepuestoI(int[] range, int idTR) {
+        String consulta = "SELECT n FROM Repuestosxempresas n LEFT JOIN Repuestos h where n.fkidRepuestos.idRepuestos = h.idRepuestos and h.fkidLineaRepuestos.idLineaRepuestos = :idLinea";
+        Query q = getEntityManager().createQuery(consulta);
+        q.setParameter("idLinea", idTR); //Variable a pasar de la sesión
+        q.setMaxResults(range[1] - range[0] + 1);
+        q.setFirstResult(range[0]);
+        return q.getResultList();
     }
     
 }
